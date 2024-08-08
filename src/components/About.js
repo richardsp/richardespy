561273237617400e4051f25a716dd7bb10b112f5
Richard Espy <richardsp@mac.com>
2024-08-08 08:49:30 -0700

    Updated home page

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,9 @@ const About = () => {
       <img src={profilePic} alt="Richard Espy" />
       <h1>About Me</h1>
       <p>
-        Welcome to my corner of the internet! I’m Richard Espy, a Senior Software QA Engineer based in Los Angeles, California. 
-        With a career spanning over two decades, I’ve honed my expertise in software analysis, quality assurance, and team management, 
-        specializing in Internet technologies. My professional journey is driven by a relentless pursuit of excellence and a passion for making 
-        the digital world a better place.
+        Hi! I&apos;m Richard Espy, a Software QA Engineer based in Los Angeles, California. 
+        With a career spanning over two decades, I&apos;ve honed my expertise in software analysis, quality assurance, and team management, 
+        specializing in Internet technologies. My professional journey is driven by a relentless pursuit of excellence and a passion for making the digital world a better place.
       </p>
       <p>
         But there&apos;s more to me than just lines of code and test cases. I&apos;m a proud father of four, a dedicated runner on a mission to conquer a marathon, 
@@ -20,7 +19,7 @@ const About = () => {
         I find immense satisfaction in the art of culinary creation.
       </p>
       <p>
-        This site is a space where I share my professional achievements, culinary adventures, and personal musings. Here, you’ll find my resume, recipes, and travel experiences. It’s a reflection of who I am – a tech enthusiast, a creative cook, and a perpetual optimist.
+        This site is a space where I share my professional achievements, culinary adventures, and personal musings. Here, you&apos;ll find my resume, recipes, and travel experiences. It&apos;s a reflection of who I am – a tech enthusiast, a creative cook, and a perpetual optimist.
       </p>
       <p>
         Thanks for stopping by! Feel free to explore, connect, and share your thoughts. Whether you’re here for tech insights, cooking tips, or just to say hello, 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className="section">
       <h2>Contact</h2>
-      <p>Feel free to reach out to me via email at <a href="mailto:richard.espy@gmail.com">richard.espy@gmail.com</a> or connect with me on <a href="https://www.linkedin.com/in/richardespy/">LinkedIn</a>.</p>
+      <p>Feel free to reach out to me via email at <a href="mailto:richard@richardespy.com">richard@richardespy.com</a> or connect with me on <a href="https://www.linkedin.com/in/richardespy/">LinkedIn</a>.</p>
     </section>
   );
 }
