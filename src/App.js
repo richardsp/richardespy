70937ae29edfecd1ef4b1186ca43395eb85fd34b
Richard Espy <richardsp@mac.com>
2024-08-13 10:47:03 -0700

    Updated resume

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -7,25 +8,49 @@ import Cooking from './components/Cooking';
 import StuffILike from './components/StuffILike';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+// import Home from './components/Home';  // Assuming you have a Home component
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <About />
-        <Resume />
-        <Cooking />
-        <StuffILike />
-        <Contact />
+    <Router>
+      <div className="App">
+        <Header />
+        <nav>
+          <ul>
+            {/* <li>
+              <Link to="/">Home</Link>
+            </li> */}
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+            <li>
+              <Link to="/cooking">Cooking</Link>
+            </li>
+            <li>
+              <Link to="/stuff-i-like">Stuff I Like</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/cooking" element={<Cooking />} />
+            <Route path="/stuff-i-like" element={<StuffILike />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
-// Todo
-// Home Page
-// Menu - About - Resume - Stuff I like - What I'm cooking
