9c1b3010a08d3c23339a4083dd1a352b4324daa2
Richard Espy <richardsp@mac.com>
2024-08-21 10:46:41 -0700

    changed CSS to have pretty tables.

--- src/components/Cooking.js
+++ src/components/Cooking.js
@@ -24,12 +24,13 @@ const recipes = [
   // Add more recipes here...
 ];
 
+
 function Cooking() {
   return (
     <section id="cooking" className="section">
       <h2>Cooking</h2>
       <p>Here are the things I'm making, the things I have made, and recipes I have on repeat.</p>
-      <table className="recipe-table">
+      <table className="scrollable-table">
         <thead>
           <tr>
             <th>Made?</th>
--- src/components/WhatILearned.js
+++ src/components/WhatILearned.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
 const stuffImWorkingOn = [
-    { id: 1, date: "August, 19, 2024", type: 'Cooking', whatItIs: 'Butter Toffee' },
-    { id: 2, date: "August, 19, 2024", type: 'Development', whatItIs: 'Updated CSS to make this site prettier' }
-    // Add more recipes here...
-  ];
-
+  { id: 1, date: "August, 19, 2024", type: 'Cooking', whatItIs: 'I made 4 batches of butter toffee before I got it right.' },
+  { id: 2, date: "August, 19, 2024", type: 'Development', whatItIs: 'I updated CSS to make this site prettier.' },
+  // ... add more entries here
+];
 
 function WhatILearned() {
   return (
     <section id="whatilearned" className="section">
       <h2>What I learned today</h2>
       <p>This is what I'm working on.</p>
-      <thead>
-        <tr>
+      <table className="scrollable-table">
+        <thead>
+          <tr>
             <th>Date</th>
             <th>Type</th>
             <th>What It Is</th>
-        </tr>
+          </tr>
         </thead>
-        
         <tbody>
           {stuffImWorkingOn.map((workingOn) => (
             <tr key={workingOn.id}>
@@ -29,10 +28,9 @@ function WhatILearned() {
             </tr>
           ))}
         </tbody>
-   </section>
+      </table>
+    </section>
   );
 }
 
 export default WhatILearned;
-
-
