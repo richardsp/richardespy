6202090a080281c29e3ba1c2a41e8ac00e0891c2
Richard Espy <richardsp@mac.com>
2024-08-21 19:12:16 -0700

    changed CSS to have pretty tables.

--- src/components/WhatILearned.js
+++ src/components/WhatILearned.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const stuffImWorkingOn = [
-  { id: 1, date: "August, 19, 2024", type: 'Cooking', whatItIs: 'I made 4 batches of butter toffee before I got it right.' },
-  { id: 2, date: "August, 19, 2024", type: 'Development', whatItIs: 'I updated CSS to make this site prettier.' },
+  { id: 1, date: "August 19, 2024", type: 'Cooking', whatItIs: 'I made 4 batches of butter toffee before I got it right.' },
+  { id: 2, date: "August 19, 2024", type: 'Development', whatItIs: 'I updated CSS to make this site prettier.' },
+  { id:31, date: "August 21, 2024", type: 'Cooking', whatItIs: 'Started to dry brine a top sirloin for the pellet cooker.' },
   // ... add more entries here
 ];
 
 function WhatILearned() {
   return (
     <section id="whatilearned" className="section what-i-learned-container">
-      <h2>What I learned today</h2>
-      <p>This is what I'm working on.</p>
-      <table className="scrollable-table">
+      <h2>What I worked on today</h2>
+      <table className="scrollable-table learned-table">
         <thead>
           <tr>
             <th>Date</th>
-            <th>Type</th>
-            <th>What It Is</th>
+            <th>Topic</th>
+            <th>What I did</th>
           </tr>
         </thead>
         <tbody>
