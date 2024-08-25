b8e104ddb5da87403d6f4bf3f75c0c3c3507f65e
Richard Espy <richardsp@mac.com>
2024-08-25 16:35:01 -0700

    changed CSS to have pretty tables.

--- src/components/WhatILearned.js
+++ src/components/WhatILearned.js
@@ -3,7 +3,8 @@ import React from 'react';
 const stuffImWorkingOn = [
   { id: 1, date: "August 19, 2024", type: 'Cooking', whatItIs: 'I made 4 batches of butter toffee before I got it right.' },
   { id: 2, date: "August 19, 2024", type: 'Development', whatItIs: 'I updated CSS to make this site prettier.' },
-  { id:31, date: "August 21, 2024", type: 'Cooking', whatItIs: 'Started to dry brine a top sirloin for the pellet cooker.' },
+  { id:3, date: "August 21, 2024", type: 'Cooking', whatItIs: 'Started to dry brine a top sirloin for the pellet cooker.' },
+  { id:3, date: "August 23, 2024", type: 'Development', whatItIs: 'Continued tweaking CSS.' },
   // ... add more entries here
 ];
 
