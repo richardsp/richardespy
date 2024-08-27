50b91826859586cf0bbff9bbb5d1d2152a45714e
Richard Espy <richardsp@mac.com>
2024-08-26 20:18:38 -0700

    Updated what I learned.

--- src/components/WhatILearned.js
+++ src/components/WhatILearned.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const stuffImWorkingOn = [
-  { id: 1, date: "August 19, 2024", type: 'Cooking', whatItIs: 'I made 4 batches of butter toffee before I got it right.' },
-  { id: 2, date: "August 19, 2024", type: 'Development', whatItIs: 'I updated CSS to make this site prettier.' },
+  { id: 1, date: "August 19, 2024", type: 'Cooking', whatItIs: 'Made 4 batches of butter toffee before I got it right.' },
+  { id: 2, date: "August 19, 2024", type: 'Development', whatItIs: 'Updated CSS to make this site prettier.' },
   { id:3, date: "August 21, 2024", type: 'Cooking', whatItIs: 'Started to dry brine a top sirloin for the pellet cooker.' },
-  { id:3, date: "August 23, 2024", type: 'Development', whatItIs: 'Continued tweaking CSS.' },
+  { id:3, date: "August 23, 2024", type: 'Development', whatItIs: 'Continued tweaking CSS. Added sortability to recipies.' },
+  { id:4, date: "August 26, 2024", type: 'Cooking', whatItIs: 'Turned my top sirloin leftovers into tacos with a spicy avocado crema. Added sortability to recipies.' },
   // ... add more entries here
 ];
 
